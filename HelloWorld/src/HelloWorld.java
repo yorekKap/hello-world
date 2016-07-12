6163acf9b86cccac581585a3d44b472c1bc3317e
YuriKap <yorek.kap@gmail.com>
2016-07-12 18:47:52 +0300

    Add simple method to main.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
 	public static void main(String[] args){
 		
 		System.out.println("hello world");
+		doSomething();
+	}
+	
+	public static void doSomething(){
+		
+		System.out.println("I'm doing something");
 		
 	}
 	
